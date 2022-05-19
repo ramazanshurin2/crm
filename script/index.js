9b6cedb6f831c35d2c2af4dd582371f80e90fab6
Ramazan <ramazanshurin2@gmail.com>
2022-05-19 17:00:04 +0600

    third commit

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const headerTextElement = document.querySelector('.modal__title');
+const formElement = document.querySelector('.modal__form');
+const checkBoxElement = document.querySelector('.modal__checkbox');
+const checkBoxLabelElement = document.querySelector('.modal__input_discount');
+
+console.log(headerTextElement);
+console.log(formElement);
+console.log(checkBoxElement);
+console.log(checkBoxLabelElement);
